e6434118b7505f497231c07f858c7ee6aebd32d5
feiyeur <fishyua@outlook.com>
2025-08-09 16:29:29 +0800

    fish.svg centered
    i love flex

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,9 @@
 import FishSymbol from "@/assets/fish.svg";
 
 export default function Home() {
-  return <FishSymbol />;
+  return (
+    <main className="overflow-clip absolute top-0 w-full h-dvh flex items-center justify-center">
+      <FishSymbol className="rotate-18" />
+    </main>
+  );
 }
