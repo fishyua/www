49ebf8773ca0d44fec0e9c693ff8589aa4e0eb28
feiyeur <fishyua@outlook.com>
2025-08-09 16:43:33 +0800

    Sticky navbar
    Correct fix to the previous one

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import FishSymbol from "@/assets/fish.svg";
 
 export default function Home() {
   return (
-    <main className="pointer-events-none overflow-clip absolute top-0 w-full h-dvh flex items-center justify-center">
+    <main className="overflow-clip absolute top-0 w-full h-dvh flex items-center justify-center">
       <FishSymbol className="rotate-18" />
     </main>
   );
--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -2,7 +2,9 @@ import { PropsWithChildren } from "react";
 import Link from "next/link";
 
 export const Navbar = (props: PropsWithChildren) => (
-  <nav className="flex items-center p-4 gap-4">{props.children}</nav>
+  <nav className="z-9999 sticky top-0 flex items-center p-4 gap-4 bg-linear-to-b from-background to-transparent">
+    {props.children}
+  </nav>
 );
 
 interface NavbarItemProps extends PropsWithChildren {
